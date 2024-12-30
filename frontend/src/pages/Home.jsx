17bf94b437c376c752b9ecfb5ea20b337c847bdb
Matthew Campbell <m.campbell92@gmail.com>
2024-12-30 22:04:31 +0100

    initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "mallee-ui";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar variant="with-branding-sticky-fade" logo="MATTHEW CAMPBELL" />
+    </div>
+  );
+}
