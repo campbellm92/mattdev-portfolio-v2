f2a8e3f96fdb1eafcca00476cce2f92635ab7de8
Matthew Campbell <m.campbell92@gmail.com>
2024-12-31 18:51:17 +0100

    import styles

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
+import styles from "./home.module.css";
 import { Navbar } from "mallee-ui";
+import Hero from "../../sections/hero/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar variant="with-branding-sticky-fade" logo="MATTHEW CAMPBELL" />
-    </div>
+      <div className={styles.container}>
+        <Hero />
+      </div>
+    </>
   );
 }
