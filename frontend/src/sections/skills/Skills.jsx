ae0b2a0b61e72758f0bea793ac08ed284113df58
Matthew Campbell <m.campbell92@gmail.com>
2025-01-08 15:19:14 +0100

    add scrolling skills component

---
+++ frontend/src/sections/skills/Skills.jsx
@@ -0,0 +1,18 @@
+import styles from "./skills.module.css";
+import icons from "../../assets/icons";
+
+export default function Skills() {
+  const iconEntries = Object.entries(icons);
+  const duplicatedIcons = [...iconEntries, ...iconEntries];
+  return (
+    <div className={styles.scrollWrapper}>
+      <div className={styles.scrollContainer}>
+        {duplicatedIcons.map(([name, src], index) => (
+          <div key={`${name}-${index}`} className={styles.scrollElement}>
+            <img className={styles.icon} src={src} alt={`${name} logo`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
