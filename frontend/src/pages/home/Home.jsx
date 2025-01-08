44908d5c9eea7656bbd18593f505437e852c805d
Matthew Campbell <m.campbell92@gmail.com>
2025-01-08 15:17:14 +0100

    add containers for projects and skills sections

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,18 @@
 import styles from "./home.module.css";
 import { Navbar } from "mallee-ui";
 import Hero from "../../sections/hero/Hero";
+import Skills from "../../sections/skills/Skills";
 
 export default function Home() {
   return (
     <>
       <Navbar variant="with-branding-sticky-fade" logo="MATTHEW CAMPBELL" />
-      <div className={styles.container}>
+      <div className={styles.heroContainer}>
         <Hero />
+      </div>
+      <div className={styles.projectsContainer}></div>
+      <div className={styles.skillsContainer}>
+        <Skills />
       </div>
     </>
   );
