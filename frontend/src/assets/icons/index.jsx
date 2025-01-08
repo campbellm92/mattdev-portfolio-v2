e8da016cca133fdf2e76eca7699c40f0eb61ed34
Matthew Campbell <m.campbell92@gmail.com>
2025-01-08 15:16:46 +0100

    add and export icons

---
+++ frontend/src/assets/icons/index.jsx
@@ -0,0 +1,45 @@
+import azureDevOpsLogo from "./azuredevops-original.svg";
+import bootstrapLogo from "./bootstrap-original.svg";
+import css3Logo from "./css3-plain.svg";
+import expressLogo from "./express-original.svg";
+import figmaLogo from "./figma-original.svg";
+import gitLogo from "./git-original.svg";
+import handlebarsLogo from "./handlebars-original.svg";
+import herokuLogo from "./heroku-plain.svg";
+import html5Logo from "./html5-plain.svg";
+import insomniaLogo from "./insomnia-original.svg";
+import javascriptLogo from "./javascript-original.svg";
+import jsonLogo from "./json-plain.svg";
+import markdownLogo from "./markdown-original.svg";
+import mysqlLogo from "./mysql-plain-wordmark.svg";
+import nodejsLogo from "./nodejs-original-wordmark.svg";
+import npmLogo from "./npm-original-wordmark.svg";
+import reactLogo from "./react-original.svg";
+import slackLogo from "./slack-original.svg";
+import storybookLogo from "./storybook-original.svg";
+import tailwindcssLogo from "./tailwindcss-original.svg";
+import vitejsLogo from "./vitejs-original.svg";
+
+export default {
+  azureDevOps: azureDevOpsLogo,
+  bootstrap: bootstrapLogo,
+  css3: css3Logo,
+  express: expressLogo,
+  figma: figmaLogo,
+  git: gitLogo,
+  handlebars: handlebarsLogo,
+  heroku: herokuLogo,
+  html5: html5Logo,
+  insomnia: insomniaLogo,
+  javascript: javascriptLogo,
+  json: jsonLogo,
+  markdown: markdownLogo,
+  mysql: mysqlLogo,
+  nodejs: nodejsLogo,
+  npm: npmLogo,
+  react: reactLogo,
+  slack: slackLogo,
+  storybook: storybookLogo,
+  tailwindcss: tailwindcssLogo,
+  vitejs: vitejsLogo,
+};
