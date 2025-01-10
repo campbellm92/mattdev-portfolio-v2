744c6db16150e27e5819eb11eb247527ae30e822
Matthew Campbell <m.campbell92@gmail.com>
2025-01-10 22:22:53 +0100

    initial setup

---
+++ frontend/src/sections/services/Services.jsx
@@ -0,0 +1,11 @@
+import styles from "./services.module.css";
+
+export default function Services() {
+  return (
+    <>
+      <div className={styles.headerContainer}>
+        <h1>Services</h1>
+      </div>
+    </>
+  );
+}
