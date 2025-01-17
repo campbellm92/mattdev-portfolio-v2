6f1a6e5d6d847761791a5fe34bea0db69a4ad446
Matthew Campbell <m.campbell92@gmail.com>
2025-01-17 10:25:58 +0100

    comment out wrapper to test

--- frontend/src/sections/projects/Projects.jsx
+++ frontend/src/sections/projects/Projects.jsx
@@ -8,32 +8,32 @@ export default function Projects() {
       <div className={styles.headerContainer}>
         <h1 className={styles.header}>Progetti</h1>
       </div>
-      <div className={styles.cardGridWrapper}>
-        <div className={styles.cardGrid}>
-          {projectData.map((project, index) => (
-            <a key={index} href={project.href}>
-              <Card
-                variant="with-image-transition"
-                image={<img src={project.img} alt={project.title} />}
-              >
-                <Card.Title>{project.title}</Card.Title>
-                <Card.Content>{project.content}</Card.Content>
-                <div className={styles.badges}>
-                  {project.badges.map((badge, badgeIndex) => (
-                    <Badge
-                      key={badgeIndex}
-                      variant={badge.variant}
-                      color={badge.color}
-                    >
-                      {badge.children}
-                    </Badge>
-                  ))}
-                </div>
-              </Card>
-            </a>
-          ))}
-        </div>
+      {/* <div className={styles.cardGridWrapper}> */}
+      <div className={styles.cardGrid}>
+        {projectData.map((project, index) => (
+          <a key={index} href={project.href}>
+            <Card
+              variant="with-image-transition"
+              image={<img src={project.img} alt={project.title} />}
+            >
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Content>{project.content}</Card.Content>
+              <div className={styles.badges}>
+                {project.badges.map((badge, badgeIndex) => (
+                  <Badge
+                    key={badgeIndex}
+                    variant={badge.variant}
+                    color={badge.color}
+                  >
+                    {badge.children}
+                  </Badge>
+                ))}
+              </div>
+            </Card>
+          </a>
+        ))}
       </div>
+      {/* </div> */}
     </>
   );
 }
