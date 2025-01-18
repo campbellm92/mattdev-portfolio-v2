d9fa9838e28c2b4a049676dd01b768525e7a20d3
Matthew Campbell <m.campbell92@gmail.com>
2025-01-18 11:51:27 +0100

    add container div

--- frontend/src/sections/skills/Skills.jsx
+++ frontend/src/sections/skills/Skills.jsx
@@ -5,13 +5,15 @@ export default function Skills() {
   const iconEntries = Object.entries(icons);
   const duplicatedIcons = [...iconEntries, ...iconEntries];
   return (
-    <div className={styles.scrollWrapper}>
-      <div className={styles.scrollContainer}>
-        {duplicatedIcons.map(([name, src], index) => (
-          <div key={`${name}-${index}`} className={styles.scrollElement}>
-            <img className={styles.icon} src={src} alt={`${name} logo`} />
-          </div>
-        ))}
+    <div className={styles.skillsContainer}>
+      <div className={styles.scrollWrapper}>
+        <div className={styles.scrollContainer}>
+          {duplicatedIcons.map(([name, src], index) => (
+            <div key={`${name}-${index}`} className={styles.scrollElement}>
+              <img className={styles.icon} src={src} alt={`${name} logo`} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
