e5a40667a66699533f56d60f9a66d2abf2c49770
Matthew Campbell <m.campbell92@gmail.com>
2025-01-18 16:04:19 +0100

    add id to section

--- frontend/src/sections/contact/Contact.jsx
+++ frontend/src/sections/contact/Contact.jsx
@@ -4,7 +4,7 @@ import styles from "./contact.module.css";
 
 export default function Contact() {
   return (
-    <div className={styles.contactSection}>
+    <div className={styles.contactSection} id="contactSection">
       <ContactDetails />
       <ContactForm />
     </div>
