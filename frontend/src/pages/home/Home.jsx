1c19fdcd9d6d451ae3ade9b78d19772a2143f90b
Matthew Campbell <m.campbell92@gmail.com>
2025-03-01 12:00:40 +0100

    insert icon

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,20 @@
+import { useState, useEffect } from "react";
 import { Navbar } from "mallee-ui";
 import Hero from "../../sections/hero/Hero";
 import Projects from "../../sections/projects/Projects";
 // import Skills from "../../sections/skills/Skills";
 // import Services from "../../sections/services/Services";
 import Contact from "../../sections/contact/Contact";
-// import { FaMoon } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
-      <Navbar variant="with-branding-sticky-fade" logo="MATTHEW CAMPBELL"/>
+      <Navbar
+        variant="with-branding-sticky-fade"
+        logo="MATTHEW CAMPBELL"
+        icons={[<FaMoon key="moon" />]}
+      />
       <div>
         <Hero />
       </div>
