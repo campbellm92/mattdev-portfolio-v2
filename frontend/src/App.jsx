8a7b44e2ed6b85af45ce91a3b26023f1ba22b20e
Matthew Campbell <m.campbell92@gmail.com>
2025-04-15 16:44:29 +0200

    add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import SingleProjectPage from "./pages/projects/SingleProjectPage";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:id" element={<SingleProjectPage />} />
+      </Routes>
+    </Router>
   );
 }
 
