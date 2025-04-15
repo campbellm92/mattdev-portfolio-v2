2ba32f81c94d81d32e8cf2be5fb72ad0bb93c681
Matthew Campbell <m.campbell92@gmail.com>
2025-04-15 16:45:29 +0200

    add mock data to new features and categories arrays

--- frontend/src/data/projectData.js
+++ frontend/src/data/projectData.js
@@ -8,18 +8,33 @@ import tastrailsImage from "../assets/project-images/tastrails.webp";
 
 const projectData = [
   {
+    id: "localis",
     href: "https://localis-capstone-f7a22eb1b92e.herokuapp.com/",
     title: "Localis",
     description:
       "Progetto stage: Dashboard di visualizzazione dei dati per l'industria del turismo.",
     features: [
-      "Dashboard personalizzata con React e Tailwind",
-      "Grafici con Recharts",
-      "Gestione complessa dello stato con React Context API",
-      "Backend performante in Node.js/Express",
-      "Autenticazione con JWT",
+      {
+        title: "Dashboard UI",
+        description: "blah blah",
+        img: localisImage,
+      },
     ],
-    img: localisImage,
+    challenges: [
+      {
+        title: "Challenge 1",
+        description: "blah blah bla bla",
+        img: localisImage,
+      },
+    ],
+    // features: [
+    //   "Dashboard personalizzata con React e Tailwind",
+    //   "Grafici con Recharts",
+    //   "Gestione complessa dello stato con React Context API",
+    //   "Backend performante in Node.js/Express",
+    //   "Autenticazione con JWT",
+    // ],
+    mainImg: localisImage,
     badges: [
       { children: "React", variant: "filled", color: "secondary" },
       { children: "Tailwind", variant: "filled", color: "secondary" },
@@ -34,16 +49,24 @@ const projectData = [
     status: "live",
   },
   {
+    id: "mallee",
     href: "https://github.com/campbellm92/mallee-ui",
     title: "Mallee-UI",
     description:
       "La mia UI library, che sto costantemente aggiungendo e perfezionando. Questo sito è costruito con Mallee!",
     features: [
-      "Libreria di componenti UI personalizzati in React",
-      "test visivo dei componenti con Storybook",
-      "Progetto open source su NPM",
+      {
+        title: "Libreria di UI personalizzati in React",
+        description: "Description description",
+        img: localisImage,
+      },
     ],
-    img: malleeImage,
+    // features: [
+    //   "Libreria di componenti UI personalizzati in React",
+    //   "test visivo dei componenti con Storybook",
+    //   "Progetto open source su NPM",
+    // ],
+    mainImg: malleeImage,
     badges: [
       { children: "TypeScript", variant: "filled", color: "secondary" },
       { children: "React", variant: "filled", color: "secondary" },
@@ -55,16 +78,24 @@ const projectData = [
     status: "in development",
   },
   {
+    id: "curlew",
     href: "https://curlew.mattdev.it/",
     title: "Curlew",
     description: "Una piattaforma di markdown blogging minimalista.",
     features: [
-      "Template HTML puri",
-      "JavaScript per il caricamento dei post",
-      "stile con CSS vanilla",
-      "syntax highlighting per il codice",
+      {
+        title: "Templating HTML puri",
+        description: "Description description",
+        img: localisImage,
+      },
     ],
-    img: curlewImage,
+    // features: [
+    //   "Template HTML puri",
+    //   "JavaScript per il caricamento dei post",
+    //   "stile con CSS vanilla",
+    //   "syntax highlighting per il codice",
+    // ],
+    mainImg: curlewImage,
     badges: [
       { children: "JavaScript", variant: "filled", color: "secondary" },
       { children: "Node", variant: "filled", color: "secondary" },
@@ -76,17 +107,25 @@ const projectData = [
     status: "github",
   },
   {
+    id: "world-happiness-rankings",
     href: "https://github.com/campbellm92/world-happiness-rankings",
     title: "World Happiness Rankings",
     description:
       "Progetto di visualizzazione dei dati completato durante il mio diploma universitario.",
     features: [
-      "Tabelle e grafici a linee",
-      "fetch dei dati da una REST API",
-      "autenticazione con JSON Web Token",
-      "UI con componenti React-Bootstrap",
+      {
+        title: "Tabelle e grafici a linee",
+        description: "Description description",
+        img: localisImage,
+      },
     ],
-    img: whrImage,
+    // features: [
+    //   "Tabelle e grafici a linee",
+    //   "fetch dei dati da una REST API",
+    //   "autenticazione con JSON Web Token",
+    //   "UI con componenti React-Bootstrap",
+    // ],
+    mainImg: whrImage,
     badges: [
       { children: "React", variant: "filled", color: "secondary" },
       { children: "React-Bootstrap", variant: "filled", color: "secondary" },
@@ -97,14 +136,21 @@ const projectData = [
     category: "university",
     status: "github",
   },
-
   {
+    id: "bookcycle",
     href: "https://campbellm92.github.io/bookcycle/",
     title: "BookCycle",
     description:
       "Libreria online, realizzata con Bootstrap. Completato per il corso universitario.",
-    features: ["Mockup statico realizzato con HTML, CSS e Bootstrap"],
-    img: bookcycleImage,
+    features: [
+      {
+        title: "Mockup statico realizzato con HTML, CSS e Bootstrap",
+        description: "Description description",
+        img: localisImage,
+      },
+    ],
+    // features: ["Mockup statico realizzato con HTML, CSS e Bootstrap"],
+    mainImg: bookcycleImage,
     badges: [
       { children: "Bootstrap", variant: "filled", color: "secondary" },
       { children: "HTML", variant: "filled", color: "secondary" },
@@ -115,17 +161,25 @@ const projectData = [
   },
 
   {
+    id: "popcornbase",
     href: "https://github.com/campbellm92/movies-app",
     title: "PopcornBase",
     description:
       "Database di film completato come parte della mia formazione backend (Node, Express) all'università.",
+    // features: [
+    //   "Template Handlebars con HTML e CSS personalizzati",
+    //   "backend in Express",
+    //   "database MySQL con query tramite Knex.js",
+    //   "autenticazione con JWT",
+    // ],
     features: [
-      "Template Handlebars con HTML e CSS personalizzati",
-      "backend in Express",
-      "database MySQL con query tramite Knex.js",
-      "autenticazione con JWT",
+      {
+        title: "Template Handlebars con HTML e CSS personalizzati",
+        description: "Description description",
+        img: localisImage,
+      },
     ],
-    img: popcornbaseImage,
+    mainImg: popcornbaseImage,
     badges: [
       { children: "Node", variant: "filled", color: "secondary" },
       { children: "Express", variant: "filled", color: "secondary" },
@@ -140,12 +194,20 @@ const projectData = [
     status: "github",
   },
   {
+    id: "tastrails",
     href: "https://campbellm92.github.io/alltrails-clone/",
-    title: "Alltrails Clone",
+    title: "TasTrails",
     description:
       "Un clone di website di trekking Alltrails realizzato completamente con HTML e CSS.",
-    features: ["Mockup statico realizzato interamente con HTML e CSS"],
-    img: tastrailsImage,
+    // features: ["Mockup statico realizzato interamente con HTML e CSS"],
+    features: [
+      {
+        title: "Mockup statico realizzato con HTML, CSS e Bootstrap",
+        description: "Description description",
+        img: localisImage,
+      },
+    ],
+    mainImg: tastrailsImage,
     badges: [
       { children: "HTML", variant: "filled", color: "secondary" },
       { children: "CSS", variant: "filled", color: "secondary" },
