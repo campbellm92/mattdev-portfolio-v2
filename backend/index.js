1a329fc8d52d34abd61c005ab142ca5d28ba0aba
Matthew Campbell <m.campbell92@gmail.com>
2025-04-18 16:08:45 +0200

    try add logging/debugging to nodemailer

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,25 @@ const express = require("express");
 const path = require("path");
 const nodeMailer = require("nodemailer");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
+
+const MYGMAIL = process.env.EMAIL;
+const PASS = process.env.PASS;
+
+// console.log(MYGMAIL);
+// console.log(PASS);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://www.mattdev.it/"],
+    origin: [
+      "http://localhost:5173",
+      "https://www.mattdev.it/",
+      "https://mattdev.it",
+    ],
   })
 );
 
@@ -18,24 +28,23 @@ app.get("/api", (req, res) => {
   res.json({ message: "🦘" });
 });
 
-// serve all routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-});
-
 // receive email from frontend React form
 // don't forget to set creds in heroku config!
 app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
+
+  console.log("Received email request:", { name, email, message });
 
   try {
     const transporter = nodeMailer.createTransport({
       host: "smtp.gmail.com",
       secure: true,
       port: 465,
+      debug: true,
+      logger: true,
       auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS,
+        user: MYGMAIL,
+        pass: PASS,
       },
     });
 
@@ -51,9 +60,14 @@ app.post("/send-email", async (req, res) => {
       .status(200)
       .json({ success: true, message: "Email sent successfully!" });
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.error("Error sending email:", error.message);
     res.status(500).json({ success: false, message: "Failed to send email." });
   }
+});
+
+// serve all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
