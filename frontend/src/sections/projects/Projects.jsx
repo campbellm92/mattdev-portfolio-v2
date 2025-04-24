c2f03c653194a5eff6328654b10c2209694b24bc
Matthew Campbell <m.campbell92@gmail.com>
2025-04-24 13:58:32 +0200

    overhaul projects section

--- frontend/src/sections/projects/Projects.jsx
+++ frontend/src/sections/projects/Projects.jsx
@@ -1,19 +1,18 @@
-import styles from "./alt.module.css";
+import styles from "./alt2.module.css";
 import projectData from "../../data/projectData.js";
 import { Button, Badge } from "mallee-ui";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import useIsMobile from "../../../hooks/useIsMobile.js";
-import GitHubCalendar from "react-github-calendar";
+// import { NavLink } from "react-router-dom";
+import useIsMobile from "../../hooks/useIsMobile.js";
+// import GitHubCalendar from "react-github-calendar";
 
 export default function Projects() {
-  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [selectedCategory, setSelectedCategory] = useState("professional");
 
   // filter btns by category
-  const filteredProjects =
-    selectedCategory === "all"
-      ? projectData
-      : projectData.filter((project) => project.category === selectedCategory);
+  const filteredProjects = projectData.filter(
+    (project) => project.category === selectedCategory
+  );
 
   // set UI based on project status
   const setProjectStatus = (status) => {
@@ -28,20 +27,13 @@ export default function Projects() {
 
   return (
     <div id="projectsSection">
-      <div className={styles.sectionTitle}>
+      <div className="sectionTitle">
         <h1>Progetti</h1>
       </div>
 
+      {/* Toggle buttons */}
       <div className={styles.selectBtnsWrapper}>
         <div className={styles.selectBtns}>
-          <Button
-            variant={selectedCategory === "all" ? "filled" : "outline"}
-            size={isMobile ? "medium" : "large"}
-            className={styles.buttonCustom}
-            onClick={() => setSelectedCategory("all")}
-          >
-            TUTTI
-          </Button>
           <Button
             variant={selectedCategory === "professional" ? "filled" : "outline"}
             size={isMobile ? "medium" : "large"}
@@ -51,129 +43,101 @@ export default function Projects() {
             PROFESSIONALI
           </Button>
           <Button
-            variant={selectedCategory === "university" ? "filled" : "outline"}
+            variant={selectedCategory === "passion" ? "filled" : "outline"}
             size={isMobile ? "medium" : "large"}
             className={styles.buttonCustom}
-            onClick={() => setSelectedCategory("university")}
+            onClick={() => setSelectedCategory("passion")}
           >
-            UNIVERSITARI
-          </Button>
-          <Button
-            variant={selectedCategory === "personal" ? "filled" : "outline"}
-            size={isMobile ? "medium" : "large"}
-            className={styles.buttonCustom}
-            onClick={() => setSelectedCategory("personal")}
-          >
-            PERSONALI
-          </Button>
-          <Button
-            variant={
-              selectedCategory === "frontend mentor" ? "filled" : "outline"
-            }
-            size={isMobile ? "medium" : "large"}
-            className={styles.buttonCustom}
-            onClick={() => setSelectedCategory("frontend mentor")}
-          >
-            FRONTEND MENTOR
+            DI PASSIONE
           </Button>
         </div>
       </div>
 
+      {/* Project cards */}
       <div className={styles.projectContainer}>
         {filteredProjects.map((project, index) => {
           const statusLabel = setProjectStatus(project.status);
-
           return (
-            <Link to={`/projects/${project.id}`} key={index}>
-              <div className={styles.projectCard}>
-                <div className={styles.imgBadgeContainer}>
+            <div key={index} className={styles.projectCardsWrapper}>
+              <div className={styles.projectBgContainer}>
+                <div className={styles.cardInner}>
+                  <div className={styles.contentItems}>
+                    <h2 className={styles.projectTitle}>{project.title}</h2>
+                    <div className={styles.badgesContainer}>
+                      {project.badges.map((badge, badgeIndex) => (
+                        <Badge
+                          key={badgeIndex}
+                          variant={badge.variant}
+                          color={badge.color}
+                        >
+                          {badge.children.toUpperCase()}
+                        </Badge>
+                      ))}
+                    </div>
+                    <p className={styles.description}>{project.description}</p>
+                    <div className={styles.linkBtns}>
+                      {statusLabel && (
+                        <a href={project.href} className={styles.linkBtn}>
+                          {statusLabel}
+                        </a>
+                      )}
+                      {/* <a href="" className={styles.linkBtn}>
+                        📓 Leggi di più
+                      </a> */}
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.projectImgContainer}>
                   <img
-                    src={project.img}
+                    src={project.mainImg}
                     alt={`${project.title}`}
                     className={styles.projectImg}
                   />
                 </div>
-
-                <div className={styles.content}>
-                  <h1 className={styles.projectTitle}>{project.title}</h1>
-
-                  <div className={styles.badgeContainer}>
-                    {project.badges.map((badge, badgeIndex) => (
-                      <Badge
-                        key={badgeIndex}
-                        variant={badge.variant}
-                        color={badge.color}
-                      >
-                        {badge.children.toUpperCase()}
-                      </Badge>
-                    ))}
-                  </div>
-
-                  <p className={styles.description}>{project.description}</p>
-                  {/* 
-                  <ul className={styles.featuresList}>
-                    {project.features.map((feature, index) => (
-                      <li key={index} className={styles.projectFeature}>
-                        {feature}
-                      </li>
-                    ))}
-                  </ul> */}
-
-                  <div className={styles.statusBtns}>
-                    {statusLabel && (
-                      <a href={project.href} className={styles.statusTag}>
-                        {statusLabel}
-                      </a>
-                    )}
-                  </div>
-                </div>
               </div>
-            </Link>
+            </div>
+            // <div key={index} className={styles.projectCard}>
+            //   <div className={styles.imgBadgeContainer}>
+
+            //   </div>
+
+            //   <div className={styles.content}>
+            //     <h2 className={styles.projectTitle}>{project.title}</h2>
+
+            //     <div className={styles.badgeContainer}>
+            //       {project.badges.map((badge, badgeIndex) => (
+            //         <Badge
+            //           key={badgeIndex}
+            //           variant={badge.variant}
+            //           color={badge.color}
+            //         >
+            //           {badge.children.toUpperCase()}
+            //         </Badge>
+            //       ))}
+            //     </div>
+
+            //     <p className={styles.description}>{project.description}</p>
+
+            //     <div className={styles.statusBtns}>
+            //       {statusLabel && (
+            //         <a href={project.href} className={styles.statusTag}>
+            //           {statusLabel}
+            //         </a>
+            //       )}
+            //     </div>
+            //   </div>
+            // </div>
           );
         })}
       </div>
-      <div className={styles.githubCalendarContainer}>
-        <div className={styles.githubCalendarWrapper}>
-          <h2 className={styles.contributionsTitle}>Contributi su Github</h2>
-          <GitHubCalendar username="campbellm92" />
+      {/* {!isMobile && (
+        <div className={styles.githubCalendarContainer}>
+          <div className={styles.githubCalendarWrapper}>
+            <h2 className={styles.contributionsTitle}>Contributi su Github</h2>
+            <GitHubCalendar username="campbellm92" />
+          </div>
         </div>
-      </div>
+      )} */}
     </div>
   );
 }
-
-// export default function Projects() {
-//   return (
-//     <div className={styles.projectsContainer}>
-//       <div className={styles.headerContainer}>
-//         <h1 className={styles.header}>Progetti</h1>
-//       </div>
-//       {/* <div className={styles.cardGridWrapper}> */}
-//       <div className={styles.cardGrid}>
-//         {projectData.map((project, index) => (
-//           <a key={index} href={project.href}>
-//             <Card
-//               variant="with-image-transition"
-//               image={<img src={project.img} alt={project.title} />}
-//             >
-//               <Card.Title>{project.title}</Card.Title>
-//               <Card.Content>{project.content}</Card.Content>
-//               <div className={styles.badges}>
-//                 {project.badges.map((badge, badgeIndex) => (
-//                   <Badge
-//                     key={badgeIndex}
-//                     variant={badge.variant}
-//                     color={badge.color}
-//                   >
-//                     {badge.children}
-//                   </Badge>
-//                 ))}
-//               </div>
-//             </Card>
-//           </a>
-//         ))}
-//       </div>
-//       {/* </div> */}
-//     </div>
-//   );
-// }
