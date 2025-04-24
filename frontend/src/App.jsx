97667c9241e81dd6959169e3f80d5c2416864b39
Matthew Campbell <m.campbell92@gmail.com>
2025-04-24 12:26:28 +0200

    remove transition-style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SingleProjectPage from "./pages/projects/SingleProjectPage";
+import "transition-style";
 
 function App() {
   return (
