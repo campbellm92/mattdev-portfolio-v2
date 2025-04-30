1f3ed2e870aef227d4b9d5adce2c2107275402a0
Matthew Campbell <m.campbell92@gmail.com>
2025-04-30 17:47:04 +0200

    replace features and challenges props with children;

--- frontend/src/layouts/SingleProjectLayout.jsx
+++ frontend/src/layouts/SingleProjectLayout.jsx
@@ -4,18 +4,16 @@ import styles from "./single-project-layout.module.css";
 export default function SingleProjectLayout({
   title,
   tech,
-  description,
   role,
   mainImg,
-  features,
-  challenges,
+  children,
 }) {
   return (
     <>
       <div className={styles.mainInfoContainer}>
         <h1>{title}</h1>
         <div>{tech}</div>
-        <p>{description}</p>
+        <p>{children}</p>
         <p>{role}</p>
       </div>
 
@@ -23,25 +21,7 @@ export default function SingleProjectLayout({
         <img src={mainImg} alt="Project main img" />
       </div>
 
-      <div className={styles.featuresContainer}>
-        {features?.map((feature, index) => (
-          <div key={index}>
-            <h1>{feature.title}</h1>
-            <p>{feature.description}</p>
-            <img src={feature.img} alt={feature.title} />
-          </div>
-        ))}
-      </div>
-
-      <div className={styles.challengesContainer}>
-        {challenges?.map((challenge, index) => (
-          <div key={index}>
-            <h1>{challenge.title}</h1>
-            <p>{challenge.description}</p>
-            <img src={challenge.img} alt={challenge.title} />
-          </div>
-        ))}
-      </div>
+      <div className={styles.projectBody}>{children}</div>
     </>
   );
 }
@@ -55,17 +35,5 @@ SingleProjectLayout.propTypes = {
   description: PropTypes.string,
   role: PropTypes.string,
   mainImg: PropTypes.string,
-  features: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      description: PropTypes.string,
-      img: PropTypes.string,
-    })
-  ),
-  challenges: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      description: PropTypes.string,
-    })
-  ),
+  children: PropTypes.node,
 };
