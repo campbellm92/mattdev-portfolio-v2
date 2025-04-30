333e69f8c2ccadf6977fa86ae33906f9c85ce6ef
Matthew Campbell <m.campbell92@gmail.com>
2025-04-30 17:46:37 +0200

    remove features and challenges mockup data from projectData

--- frontend/src/data/projectData.js
+++ frontend/src/data/projectData.js
@@ -11,27 +11,6 @@ const projectData = [
     title: "Indisciplinato",
     description:
       "Landing page per un evento per produttori di vino indipendenti",
-    features: [
-      {
-        title: "",
-        description: "blah blah",
-        img: localisImage,
-      },
-    ],
-    challenges: [
-      {
-        title: "Challenge 1",
-        description: "blah blah bla bla",
-        img: localisImage,
-      },
-    ],
-    // features: [
-    //   "Dashboard personalizzata con React e Tailwind",
-    //   "Grafici con Recharts",
-    //   "Gestione complessa dello stato con React Context API",
-    //   "Backend performante in Node.js/Express",
-    //   "Autenticazione con JWT",
-    // ],
     mainImg: indisciplinatoImage,
     badges: [
       { children: "JavaScript", variant: "filled", color: "secondary" },
@@ -49,27 +28,6 @@ const projectData = [
     title: "Localis Dashboard",
     description:
       "Progetto stage: Dashboard di visualizzazione dei dati per l'industria del turismo.",
-    features: [
-      {
-        title: "Dashboard UI",
-        description: "blah blah",
-        img: localisImage,
-      },
-    ],
-    challenges: [
-      {
-        title: "Challenge 1",
-        description: "blah blah bla bla",
-        img: localisImage,
-      },
-    ],
-    // features: [
-    //   "Dashboard personalizzata con React e Tailwind",
-    //   "Grafici con Recharts",
-    //   "Gestione complessa dello stato con React Context API",
-    //   "Backend performante in Node.js/Express",
-    //   "Autenticazione con JWT",
-    // ],
     mainImg: localisImage,
     badges: [
       { children: "React", variant: "filled", color: "secondary" },
@@ -90,18 +48,6 @@ const projectData = [
     title: "Mallee-UI",
     description:
       "La mia UI library, che sto costantemente aggiungendo e perfezionando. Questo sito è partialmente costruito con Mallee!",
-    features: [
-      {
-        title: "Libreria di UI personalizzati in React",
-        description: "Description description",
-        img: localisImage,
-      },
-    ],
-    // features: [
-    //   "Libreria di componenti UI personalizzati in React",
-    //   "test visivo dei componenti con Storybook",
-    //   "Progetto open source su NPM",
-    // ],
     mainImg: malleeImage,
     badges: [
       { children: "TypeScript", variant: "filled", color: "secondary" },
@@ -118,19 +64,6 @@ const projectData = [
     href: "https://curlew.mattdev.it/",
     title: "Curlew",
     description: "Una piattaforma di markdown blogging minimalista.",
-    features: [
-      {
-        title: "Templating HTML puri",
-        description: "Description description",
-        img: localisImage,
-      },
-    ],
-    // features: [
-    //   "Template HTML puri",
-    //   "JavaScript per il caricamento dei post",
-    //   "stile con CSS vanilla",
-    //   "syntax highlighting per il codice",
-    // ],
     mainImg: curlewImage,
     badges: [
       { children: "JavaScript", variant: "filled", color: "secondary" },
@@ -149,14 +82,6 @@ const projectData = [
     title: "TasTrails",
     description:
       "Uno dei miei primi progetti: un clone di website di trekking Alltrails realizzato completamente con HTML e CSS.",
-    // features: ["Mockup statico realizzato interamente con HTML e CSS"],
-    features: [
-      {
-        title: "Mockup statico realizzato con HTML, CSS e Bootstrap",
-        description: "Description description",
-        img: localisImage,
-      },
-    ],
     mainImg: tastrailsImage,
     badges: [
       { children: "HTML", variant: "filled", color: "secondary" },
@@ -165,95 +90,6 @@ const projectData = [
     category: "passion",
     status: "live",
   },
-
-  // {
-  //   id: "world-happiness-rankings",
-  //   href: "https://github.com/campbellm92/world-happiness-rankings",
-  //   title: "World Happiness Rankings",
-  //   description:
-  //     "Progetto di visualizzazione dei dati completato durante il mio diploma universitario.",
-  //   features: [
-  //     {
-  //       title: "Tabelle e grafici a linee",
-  //       description: "Description description",
-  //       img: localisImage,
-  //     },
-  //   ],
-  //   // features: [
-  //   //   "Tabelle e grafici a linee",
-  //   //   "fetch dei dati da una REST API",
-  //   //   "autenticazione con JSON Web Token",
-  //   //   "UI con componenti React-Bootstrap",
-  //   // ],
-  //   mainImg: whrImage,
-  //   badges: [
-  //     { children: "React", variant: "filled", color: "secondary" },
-  //     { children: "React-Bootstrap", variant: "filled", color: "secondary" },
-  //     { children: "Recharts", variant: "filled", color: "secondary" },
-  //     { children: "AG-Grid", variant: "filled", color: "secondary" },
-  //     { children: "JSON Web Tokens", variant: "filled", color: "secondary" },
-  //   ],
-  //   category: "university",
-  //   status: "github",
-  // },
-
-  // {
-  //   id: "popcornbase",
-  //   href: "https://github.com/campbellm92/movies-app",
-  //   title: "PopcornBase",
-  //   description:
-  //     "Database di film completato come parte della mia formazione backend (Node, Express) all'università.",
-  //   // features: [
-  //   //   "Template Handlebars con HTML e CSS personalizzati",
-  //   //   "backend in Express",
-  //   //   "database MySQL con query tramite Knex.js",
-  //   //   "autenticazione con JWT",
-  //   // ],
-  //   features: [
-  //     {
-  //       title: "Template Handlebars con HTML e CSS personalizzati",
-  //       description: "Description description",
-  //       img: localisImage,
-  //     },
-  //   ],
-  //   mainImg: popcornbaseImage,
-  //   badges: [
-  //     { children: "Node", variant: "filled", color: "secondary" },
-  //     { children: "Express", variant: "filled", color: "secondary" },
-  //     { children: "MySQL", variant: "filled", color: "secondary" },
-  //     { children: "Knex.js", variant: "filled", color: "secondary" },
-  //     { children: "Handlebars", variant: "filled", color: "secondary" },
-  //     { children: "HTML", variant: "filled", color: "secondary" },
-  //     { children: "CSS", variant: "filled", color: "secondary" },
-  //     { children: "JSON Web Tokens", variant: "filled", color: "secondary" },
-  //   ],
-  //   category: "university",
-  //   status: "github",
-  // },
-
-  // {
-  //   id: "bookcycle",
-  //   href: "https://campbellm92.github.io/bookcycle/",
-  //   title: "BookCycle",
-  //   description:
-  //     "Libreria online, realizzata con Bootstrap. Completato per il corso universitario.",
-  //   features: [
-  //     {
-  //       title: "Mockup statico realizzato con HTML, CSS e Bootstrap",
-  //       description: "Description description",
-  //       img: localisImage,
-  //     },
-  //   ],
-  //   // features: ["Mockup statico realizzato con HTML, CSS e Bootstrap"],
-  //   mainImg: bookcycleImage,
-  //   badges: [
-  //     { children: "Bootstrap", variant: "filled", color: "secondary" },
-  //     { children: "HTML", variant: "filled", color: "secondary" },
-  //     { children: "CSS", variant: "filled", color: "secondary" },
-  //   ],
-  //   category: "university",
-  //   status: "live",
-  // },
 ];
 
 export default projectData;
