aedabaf1c831d69b18f71c3ae419a610910ed92c
Matthew Campbell <m.campbell92@gmail.com>
2025-05-02 16:17:25 +0200

    clean up comments

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,7 @@
-// import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Hero from "../../sections/hero/Hero";
 import Projects from "../../sections/projects/Projects";
-// import Skills from "../../sections/skills/Skills";
-// import Services from "../../sections/services/Services";
 import Contact from "../../sections/contact/Contact";
-// import { FaMoon } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -19,12 +15,6 @@ export default function Home() {
         <Projects />
       </div>
       <hr />
-      {/* <div className={styles.skillsContainer}>
-        <Skills />
-      </div> */}
-      {/* <div className={styles.servicesContainer}>
-        <Services />
-      </div> */}
       <div>
         <Contact />
       </div>
