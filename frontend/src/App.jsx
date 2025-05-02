8601a38d1b4842716d8a67038e27ed01313f34e0
Matthew Campbell <m.campbell92@gmail.com>
2025-05-02 16:55:59 +0200

    reimplement react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-// import SingleProjectPage from "./pages/projects/SingleProjectPage";
+import SingleProjectPage from "./pages/projects/SingleProjectPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/projects/:id" element={<SingleProjectPage />} /> */}
+        <Route path="/projects/:id" element={<SingleProjectPage />} />
       </Routes>
     </Router>
   );
