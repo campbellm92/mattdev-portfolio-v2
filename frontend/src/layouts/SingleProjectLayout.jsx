b7e97562940c5958b17c77dfb04206d13994ea5f
Matthew Campbell <m.campbell92@gmail.com>
2025-05-02 16:56:27 +0200

    add badges to layout

--- frontend/src/layouts/SingleProjectLayout.jsx
+++ frontend/src/layouts/SingleProjectLayout.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 import styles from "./single-project-layout.module.css";
+import { Badge } from "mallee-ui";
 
 export default function SingleProjectLayout({
   title,
-  tech,
+  descriptionLong,
+  badges,
   role,
   mainImg,
   children,
@@ -12,7 +14,14 @@ export default function SingleProjectLayout({
     <>
       <div className={styles.mainInfoContainer}>
         <h1>{title}</h1>
-        <div>{tech}</div>
+        <div className={styles.badgesContainer}>
+          {badges?.map((badge, index) => (
+            <Badge key={index} variant={badge.variant} color={badge.color}>
+              {badge.children}
+            </Badge>
+          ))}
+        </div>
+        <p>{descriptionLong}</p>
         <p>{children}</p>
         <p>{role}</p>
       </div>
@@ -28,11 +37,14 @@ export default function SingleProjectLayout({
 
 SingleProjectLayout.propTypes = {
   title: PropTypes.string.isRequired,
-  tech: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.arrayOf(PropTypes.string),
-  ]),
-  description: PropTypes.string,
+  badges: PropTypes.arrayOf(
+    PropTypes.shape({
+      children: PropTypes.string.isRequired,
+      variant: PropTypes.string,
+      color: PropTypes.string,
+    })
+  ),
+  descriptionLong: PropTypes.string,
   role: PropTypes.string,
   mainImg: PropTypes.string,
   children: PropTypes.node,
