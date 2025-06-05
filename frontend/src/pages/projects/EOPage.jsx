d38c75264de31ac0e4cd3ee932e35f68437fe0d5
Matthew Campbell <m.campbell92@gmail.com>
2025-06-05 18:30:58 +0200

    add project pages

---
+++ frontend/src/pages/projects/EOPage.jsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom";
+import projectData from "../../data/projectData.jsx";
+import Nav from "../../components/Navbar.jsx";
+import SingleProjectLayout from "../../layouts/SingleProjectLayout.jsx";
+
+export default function EOPage() {
+  const { id } = useParams();
+  const project = projectData.find((p) => p.id === id);
+
+  if (!project) {
+    return <p>Project not found</p>;
+  }
+
+  return (
+    <>
+      <Nav />
+      <SingleProjectLayout
+        title={project.title}
+        descriptionLong={project.descriptionLong}
+        badges={project.badges}
+        role={project.role}
+        mainImg={project.mainImg}
+        features={project.features}
+      />
+    </>
+  );
+}
---
+++ frontend/src/pages/projects/LocalisPage.jsx
@@ -0,0 +1,28 @@
+import { useParams } from "react-router-dom";
+import projectData from "../../data/projectData.jsx";
+import Nav from "../../components/Navbar.jsx";
+import SingleProjectLayout from "../../layouts/SingleProjectLayout.jsx";
+
+export default function LocalisPage() {
+  const { id } = useParams();
+  const project = projectData.find((p) => p.id === id);
+
+  if (!project) {
+    return <p>Project not found</p>;
+  }
+
+  return (
+    <>
+      <Nav />
+      <SingleProjectLayout
+        title={project.title}
+        badges={project.badges}
+        description={project.description}
+        role={project.role}
+        mainImg={project.mainImg}
+        features={project.features}
+        challenges={project.challenges}
+      />
+    </>
+  );
+}
